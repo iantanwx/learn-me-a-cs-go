e2b3e209b25b2d190086726d13b050d925110b77
Ian Tan <iantanwx@gmail.com>
2019-02-18 12:12:10 +0800

    fix(hackerrank): optimise solution for hashmap-00005

--- hackerrank/interview_prep_kit/hashmaps/00005-freqs/solution.go
+++ hackerrank/interview_prep_kit/hashmaps/00005-freqs/solution.go
@@ -3,6 +3,7 @@ package freqs
 // Complete the freqQuery function below.
 func freqQuery(queries [][]int32) []int32 {
 	out := []int32{}
+	freqs := map[int32]int32{}
 	freqCount := map[int32]int32{}
 
 	for _, query := range queries {
@@ -11,19 +12,22 @@ func freqQuery(queries [][]int32) []int32 {
 
 		switch op {
 		case 1:
-			freqCount[v]++
+			freqs[v]++
+			freqCount[freqs[v]]++
+			if cnt := freqCount[freqs[v]-1]; cnt > 0 {
+				freqCount[freqs[v]-1]--
+			}
 		case 2:
-			if f, prs := freqCount[v]; prs && f > 0 {
-				freqCount[v]--
+			if f, prs := freqs[v]; prs && f > 0 {
+				freqs[v]--
+				freqCount[freqs[v]]++
+				freqCount[freqs[v]+1]--
 			}
 		case 3:
 			var res int32
 
-			for _, f := range freqCount {
-				if v == f {
-					res = 1
-					break
-				}
+			if f := freqCount[v]; f > 0 {
+				res = 1
 			}
 
 			out = append(out, res)
