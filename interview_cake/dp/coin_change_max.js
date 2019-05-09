725818ce44d8266f606edc55cab92f104921b466
Ian Tan <iantanwx@gmail.com>
2019-05-09 11:32:37 +0800

    fix(interview_cake): fix make change bottom-up solution

--- interview_cake/dp/coin_change_max.js
+++ interview_cake/dp/coin_change_max.js
@@ -68,14 +68,14 @@ function changePossibilitiesDP(amountLeft, denominations) {
       }
 
       if (k >= denom) {
-        dp[j][k] = 1 + Math.max(dp[j][k - 1], dp[j-1][k]);
+        dp[j][k] = dp[j-1][k] + dp[j][k-denom];
       } else {
-        dp[j][k] = Math.max(dp[j][k - 1], dp[j-1][k]);
+        dp[j][k] = dp[j-1][k];
       }
     }
   }
 
-  return dp[denominations.length - 1][amountLeft - 1];
+  return dp[denominations.length - 1][amountLeft];
 };
 
 // Tests
@@ -85,30 +85,30 @@ let actual = changePossibilities(4, [1, 2, 3]);
 let expected = 4;
 assertEqual(actual, expected, desc);
 
-// desc = 'one way to make zero cents';
-// actual = changePossibilities(0, [1, 2]);
-// expected = 1;
-// assertEqual(actual, expected, desc);
+desc = 'one way to make zero cents';
+actual = changePossibilities(0, [1, 2]);
+expected = 1;
+assertEqual(actual, expected, desc);
 
-// desc = 'no ways if no coins';
-// actual = changePossibilities(1, []);
-// expected = 0;
-// assertEqual(actual, expected, desc);
+desc = 'no ways if no coins';
+actual = changePossibilities(1, []);
+expected = 0;
+assertEqual(actual, expected, desc);
 
-// desc = 'big coin value';
-// actual = changePossibilities(5, [25, 50]);
-// expected = 0;
-// assertEqual(actual, expected, desc);
+desc = 'big coin value';
+actual = changePossibilities(5, [25, 50]);
+expected = 0;
+assertEqual(actual, expected, desc);
 
-// desc = 'big target amount';
-// actual = changePossibilities(50, [5, 10]);
-// expected = 6;
-// assertEqual(actual, expected, desc);
+desc = 'big target amount';
+actual = changePossibilities(50, [5, 10]);
+expected = 6;
+assertEqual(actual, expected, desc);
 
-// desc = 'change for one dollar';
-// actual = changePossibilities(100, [1, 5, 10, 25, 50]);
-// expected = 292;
-// assertEqual(actual, expected, desc);
+desc = 'change for one dollar';
+actual = changePossibilities(100, [1, 5, 10, 25, 50]);
+expected = 292;
+assertEqual(actual, expected, desc);
 
 function assertEqual(a, b, desc) {
   if (a === b) {
